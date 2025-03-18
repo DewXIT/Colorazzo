162cefe3aba94c8e78f8ca399cbf6af11e20724b
Marcin Falkowski <marcin@MacBook-Air.local>
2025-03-18 13:15:35 +0100

    SEO

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,48 @@
 import React, { ReactNode } from 'react'
 import Footer from './Footer'
-
+import { baseUrl } from '@/pages/sitemap'
+import { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 interface LayoutProps {
   children: ReactNode
 }
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Colorazzo',
+    template: '%s | Colorazzo',
+  },
+  description: 'Color palette web tool',
+  openGraph: {
+    title: 'Colorazzo',
+    description: 'Color palette web tool',
+    url: baseUrl,
+    siteName: 'Colorazzo',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+}
+
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col justify-between bg-[#121212] h-full min-h-screen">
       {children}
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </div>
   )
 }
---
+++ src/rss/route.ts
@@ -0,0 +1,18 @@
+import { baseUrl } from '@/pages/sitemap'
+
+export async function GET() {
+  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+    <channel>
+        <title>Colorazzo</title>
+        <link>${baseUrl}</link>
+        <description>This is Colorazzo RSS feed</description>
+    </channel>
+  </rss>`
+
+  return new Response(rssFeed, {
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+  })
+}
