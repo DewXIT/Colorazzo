68d69f95d61a9de38f51593fefb106f2b5092a45
Marcin Falkowski <marcin@MacBook-Air.local>
2025-03-18 13:18:01 +0100

    SEO

--- src/pages/sitemap.ts
+++ src/pages/sitemap.ts
@@ -1,7 +1,7 @@
 export const baseUrl = 'https://colorazzo.netlify.app/'
 
 export default async function sitemap() {
-  let routes = [''].map((route) => ({
+  const routes = [''].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
